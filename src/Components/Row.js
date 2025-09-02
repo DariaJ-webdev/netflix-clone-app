44a04198558a0767d4d678dcdfba953eb1354f39
DariaJ-webev <frontendwebcraftx@gmail.com>
2025-09-01 22:28:51 -0400

    checked video playback settings

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -4,15 +4,15 @@ import requests from '../requests';
 import axios from '../axios';
 import '../cstyles/Row.css';
 import LiteYouTubeEmbed from 'react-lite-youtube-embed';
-import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
 import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMoviews] =useState([]);
-    const [trailerUrl, setTrailerUrl] = useState(""); //whenever we click a movie, this will store the trailer URL
-// useEffect is a snippet of code that runs on a condition / variable
+    const [trailerUrl, setTrailerUrl] = useState(""); //whenever user clicks a movie, this will store the trailer URL...
+// useEffect is a snippet of code that runs on this condition 
     useEffect(() => { 
         // if [], run once when the row loads, and don't run again
         //if [movies], run every time movies changes
@@ -64,7 +64,7 @@ function Row({title, fetchUrl, isLargeRow}) {
         </div>
        {trailerUrl && (<LiteYouTubeEmbed
                 id={trailerUrl} 
-                title="A descriptive title for your video"
+                title={title}
                 adNetwork={false} 
             />
         )}
